dabe5f1a99702c81f0c4502fed652ec9280fad7e
Kunal Joshi <mail.kunaljoshi@gmail.com>
2020-06-01 11:59:15 +0530

    Loading SECRET_KEY from a json file in ~/config/trydjango_config.json

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
+
+with open('/home/kunalj/config/trydjango_config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pd_p214@mclv*&00s@lec27!7os#agf_7rzluq3^nc$6bdne^*'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
