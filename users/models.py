c3bde4c69b35ccedde09471d4759c6c7922d355c
Kunal Joshi <mail.kunaljoshi@gmail.com>
2020-06-01 15:50:22 +0530

    Added ability to resize larger images to 120x120

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.name} Profile' 
+
+    def save(self, *args, **kwargs):
+        super().save(args, kwargs)
+        img = Image(self.image.path)
+
+        if img.height > 120 or img.width > 120:
+            output_size = (120, 120)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
